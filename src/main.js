ae378404bd88aff659b832c8f8dc3db2a02367bd
pixelcat-code <pixeletedcat@gmail.com>
2026-06-17 18:21:41 +0300

    Revert "Fix board flicker from conic-gradient repaints"
    This reverts commit bc5695d72f1f26d41ff717dffd99f8f4145a110a.

--- src/main.js
+++ src/main.js
@@ -558,10 +558,7 @@ function renderTopBar(stateLike) {
     const pct = atMax ? 100 : Math.min(100, Math.round(((stateLike.hatchProgress ?? 0) / HATCH_GOAL) * 100));
     elements.rerollRun.style.setProperty("--pct", String(pct));
     if (elements.rerollHud) {
-      const pctStr = String(pct);
-      if (elements.rerollHud.style.getPropertyValue("--pct") !== pctStr) {
-        elements.rerollHud.style.setProperty("--pct", pctStr);
-      }
+      elements.rerollHud.style.setProperty("--pct", String(pct));
     }
   }
 }
